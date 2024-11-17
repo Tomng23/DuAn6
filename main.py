993a880369ae9186088f54fec5172e9185696c2f
Tomng23 <156044644+Tomng23@users.noreply.github.com>
2024-11-17 03:23:03 +0000

    Thay doi main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import streamlit as st
-import time
 
-st.title('my_progress')
+st.title('Điền Thông Tin Giới Thiệu Bản Thân')
 my_bar = st.progress(0)
 
-for percent_complete in range(100):
-    time.sleep(0.05)
-    my_bar.progress(percent_complete + 1)
+quiz = ['Ho va ten:', 'Ngay thang nam sinh:', 'So thich:']
+answers = []
+len_quiz = len(quiz)
 
-if st.button('Click ME!'):
-    st.balloons()
+for i in range(len_quiz):
+    answer = st.text_input(quiz[i], '')
+    if answer != '':
+        answers.append(answer)
+
+if st.button('Confirm'):
+    if len(answers) == len_quiz:
+        my_bar.progress(100)
+        st.write('Ban da hoan thanh day du thong tin!')
+        st.balloons()
+    else:
+        my_bar.progress(len(answers)/len_quiz)
+        st.write('Bạn chưa hoàn thành đầy đủ thông tin!')
+
+for i in range(len(answers)):
+    st.write(quiz[i], answers[i])
+        
